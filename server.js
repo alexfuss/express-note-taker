65b1882523f7da90b7528e1a22a83e400be808d4
Alex Fuss <alexfuss2@gmail.com>
2020-08-10 20:25:40 -0400

    Updated the location of the server file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 let app = express();
-let db = require("./db/db.json");
+let db = require("./Proj_files/Develop/db/db.json");
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
